a230733f2238d1c09f7a4ea22572278fd7110b7c
IndexError <lkycst@gmail.com>
2022-06-18 21:08:05 +0700

    delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from utils.lof import LOF
-from utils.metrics import euclidean
-
-lof = LOF(2, euclidean)
-a = np.array([[0,1], [1,0], [1,1], [69,420]])
-print(lof.fit_predict(a))
